2c48c8ea71e63b92084f8cef7a376d246a6c28d3
HossamAshraf <hossamashraf1692000@gmail.com>
2021-03-30 22:08:59 +0200

    blog create

---
+++ src/app/pages/blog-category/blog-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-blog-category',
+  templateUrl: './blog-category.component.html',
+  styleUrls: ['./blog-category.component.scss']
+})
+export class BlogCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
